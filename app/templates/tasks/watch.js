ce4997569653d4fe0c923c8b035c6f0e738bfd28
Stéphane Bachelier <stephane.bachelier@gmail.com>
2014-06-25 03:07:49 +0200

    chore(lint): add code check on watch

--- app/templates/tasks/watch.js
+++ app/templates/tasks/watch.js
@@ -1,14 +1,23 @@
 module.exports = {
   gruntfile: {
     files: '<%= jshint.gruntfile.src %>',
-    tasks: ['jshint:gruntfile']
+    tasks: [
+      'jshint:gruntfile',
+      'jscs:gruntfile'
+    ]
   },
   lib: {
     files: '<%= jshint.lib.src %>',
-    tasks: ['jshint:lib']
+    tasks: [
+      'jshint:lib',
+      'jscs:lib'
+    ]
   },
   test: {
     files: '<%= jshint.test.src %>',
-    tasks: ['jshint:test']
+    tasks: [
+      'jshint:test',
+      'jscs:test'
+    ]
   }
 };
