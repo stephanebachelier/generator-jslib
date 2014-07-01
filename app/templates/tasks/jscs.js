92d8810366fa001db95b5c5403723e98df4c8aba
Stéphane Bachelier <stephane.bachelier@gmail.com>
2014-07-01 11:33:19 +0200

    fix(jscs): invalid task name

--- app/templates/tasks/jscs.js
+++ app/templates/tasks/jscs.js
@@ -5,5 +5,5 @@ module.exports = {
   },
   lib: ['lib/<%%= pkg.name %>.js'],
   test: ['test/spec/{,*/}*.js'],
-  grunt: ['Gruntfile.js', 'tasks/{,*/}*.js']
+  gruntfile: ['Gruntfile.js', 'tasks/{,*/}*.js']
 };
