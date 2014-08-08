f8e7895ef52bf2c35025b605aedd63c7f0fe0ded
Stéphane Bachelier <stephane.bachelier@gmail.com>
2014-08-08 23:14:43 +0200

    fix(typo): syntax error while using wrong quote
    test driven development might have help this one :/

--- app/templates/tasks/concat.js
+++ app/templates/tasks/concat.js
@@ -4,7 +4,7 @@ module.exports = {
     stripBanners: true
   },
   dist: {
-    src: ['lib/<%%= pkg.name.replace(/\.js$/, '') %>.js'],
-    dest: 'dist/<%%= pkg.name.replace(/\.js$/, '') %>.js'
+    src: ['lib/<%%= pkg.name.replace(/\.js$/, "") %>.js'],
+    dest: 'dist/<%%= pkg.name.replace(/\.js$/, "") %>.js'
   }
 };
