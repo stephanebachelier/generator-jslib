bf338da937e47bc2acce9649ff0a24be93862ec6
Stéphane Bachelier <stephane.bachelier@gmail.com>
2015-01-15 13:47:45 +0100

    fix(banner): put banner on top of umd wrapper

--- app/templates/tasks/umd.js
+++ app/templates/tasks/umd.js
@@ -2,7 +2,7 @@ module.exports = {
   lib: {
     template: 'umd',
     indent: '  ',
-    src: 'dist/<%%= pkg.name.replace(/.js$/, "") %>.js',
+    src: 'lib/<%%= pkg.name.replace(/.js$/, "") %>.js',
     dest: 'dist/<%%= pkg.name.replace(/.js$/, "") %>.js',
     returnExportsGlobal: '<%= name %>',
     deps: {
