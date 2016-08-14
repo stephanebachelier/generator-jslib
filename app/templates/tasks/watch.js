bf70b8ac137c7e7310f51d72c7f0620842b59161
Stephane BACHELIER <stephane.bachelier@gmail.com>
2016-08-14 02:29:53 +0200

    chore(lint): switch to standard for template

--- app/templates/tasks/watch.js
+++ app/templates/tasks/watch.js
@@ -1,24 +1,8 @@
 module.exports = {
-  gruntfile: {
-    files: '<%%= jshint.gruntfile.src %>',
-    tasks: [
-      'jshint:gruntfile',
-      'jscs:gruntfile',
-      'lintspaces:gruntfile'
-    ]
-  },
   lib: {
-    files: '<%%= jshint.lib.src %>',
+    files: ['{,lib/,tasks/}*.js'],
     tasks: [
       'build'
-    ]
-  },
-  test: {
-    files: '<%%= jshint.test.src %>',
-    tasks: [
-      'jshint:test',
-      'jscs:test',
-      'lintspaces:test'
     ]
   }
 };
